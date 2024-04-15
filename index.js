a7e6df4e573638c0b74678e9511c8ef1eba00d40
Joachim Flottorp <joachimflottorp@gmail.com>
2024-04-15 12:29:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.json({response: "Hello, World!"});
 })
+
 app.listen(process.env.PORT || 3000)
